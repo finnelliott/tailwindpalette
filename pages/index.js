6f14b1ab82ef06a3a0285cc793f946ca3b9cf0cd
finnelliott <finn@finnelliott.com>
2022-10-05 13:51:09 +0100

    adding more active label for select input

--- pages/index.js
+++ pages/index.js
@@ -114,7 +114,7 @@ export default function Home() {
       </div>
       <div className="max-w-xs pb-16 mx-auto">
         <label htmlFor="primary" className="block text-sm font-medium text-gray-200 text-center w-full mb-4">
-          Primary
+          Select the primary colour for your palette
         </label>
         <select
           id="primary"
